232c3cda725940253f25ed67f2b4790a1a7ac6c7
Sebastián <spierini@info.lifia.unlp.edu.ar>
2024-05-28 15:12:18 -0300

    se agrega javascript

---
+++ api.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contenedorProductos = document.getElementById('contenedor-productos');
+    
+   
+
+    fetch('/productos.json')
+        .then((resp) => resp.json())
+        .then((data) => {
+            data.forEach((prod) => {
+                const div = document.createElement('div');
+                div.classList.add('producto');
+                div.innerHTML = `
+                    <img src="${prod.img}" alt="${prod.nombre}">
+                    <h3>${prod.nombre}</h3>
+                    <p>${prod.desc}</p>
+                    <p class="precioProducto">Precio: $${prod.precio}</p>
+               `;
+                contenedorProductos.appendChild(div);
+            });
+        })
+        .catch(error => console.error('Error al cargar los productos:', error));
+});
+
+
+
